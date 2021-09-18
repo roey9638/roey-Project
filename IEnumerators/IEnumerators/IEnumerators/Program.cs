5e275408c1d7692950d8494d31c95af96ed4691b
roey9638 <85359163+roey9638@users.noreply.github.com>
2021-09-18 13:11:52 +0300

    Add files via upload

---
+++ IEnumerators/IEnumerators/IEnumerators/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IEnumerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Enumerator Qst 01
+            Console.WriteLine("Enumerator Qst 01 ");
+            //Answer is in the [CountTo100] Class.
+            #endregion
+
+
+            #region Enumerator Qst 02
+            Console.WriteLine(" ");
+            Console.WriteLine("Enumerator Qst 02 ");
+
+            CountTo100 count = new CountTo100();
+            count.CountWithForEach();
+
+            #endregion
+
+
+            #region Enumerator Qst 03
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Enumerator Qst 03 ");
+
+            EnumeratorQst03 qst03 = new EnumeratorQst03(10);
+            qst03.CountForEach();
+
+            #endregion
+
+
+            #region Enumerator Qst 04
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Enumerator Qst 04");
+            Console.WriteLine(" ");
+
+
+            List<Student> studentss = new List<Student>()
+            {
+                new Student(90,"Gal"),
+                new Student(80,"Daniel"),
+                new Student(70,"Kantor"),
+                new Student(5,"Jonathan")
+
+            };
+
+            foreach (Student stu in new StudentsWhoParcticeAlot(studentss))
+            {
+                if(stu.Grade >= 10)
+                {
+                  Console.WriteLine($"Student Name = {stu.Name}.  Student Grade = {stu.Grade}.");
+                }
+            }
+            
+            #endregion
+
+        }
+    }
+
+    class CountTo100 : IEnumerable
+    {
+        #region Enumerator Qst 01
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                yield return i * 10;
+            }
+        }
+
+        #endregion
+
+
+        #region Enumerator Qst 02
+
+        public void CountWithForEach()
+        {
+            foreach (var item in new CountTo100())
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        #endregion
+
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    class EnumeratorQst03 : IEnumerable<int>
+    {
+        public int Num { get; set; }
+
+
+        public EnumeratorQst03(int num)
+        {
+            Num = num;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                yield return i * 10;
+            }
+        }
+
+        #region Enumerator Qst 03
+
+        public void CountForEach()
+        {
+            foreach (var item in new CountTo100())
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        #endregion
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IEnumerators/IEnumerators/IEnumerators/StudentsWhoParcticeAlot.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerators
+{
+    class StudentsWhoParcticeAlot : IEnumerable<Student>
+    {
+        public List<Student> Students { get; set; }
+
+
+        public StudentsWhoParcticeAlot(List<Student> students)
+        {
+            Students = students;
+        }
+
+
+        public IEnumerator<Student> GetEnumerator()
+        {
+            foreach (var student in Students)
+            {
+                if (student.Grade >= 10)
+                {
+                    yield return student;
+                }
+
+            }
+        }
+
+       
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class Student
+    {
+        public int Grade { get; set; }
+        public string Name { get; set; }
+
+        public Student(int grade, string name)
+        {
+            Grade = grade;
+            Name = name;
+        }
+
+    }
+}
